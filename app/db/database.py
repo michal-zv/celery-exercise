e4cc31a239b8adcada66cb12472f925916b4af35
Michal <michalzvu25@gmail.com>
2025-09-16 17:03:08 +0300

    basic db setup

---
+++ app/db/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from fastapi import Depends
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password123@localhost:5432/celery-exercise"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
