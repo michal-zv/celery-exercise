d88168cc6df20358a25cc4313e93c390ce7bd984
Michal <michalzvu25@gmail.com>
2025-09-17 19:30:42 +0300

    sum_type endpoint

--- app/crud/category.py
+++ app/crud/category.py
@@ -1,3 +1,4 @@
+from typing import List
 from sqlalchemy.orm import Session
 from app.models import Category
 from app.schemas import CategoryCreate, CategoryUpdate
@@ -7,5 +8,8 @@ class CRUDCategory(CRUDBase[Category, CategoryCreate, CategoryUpdate]):
     
     def get_category_by_name(self, db: Session, name: str) -> Category | None:
         return db.query(Category).filter(Category.name == name).first()
+    
+    def get_categories_by_type(self, db: Session, type: str) -> List[Category]:
+        return db.query(Category).filter(Category.type == type).all()
 
 category = CRUDCategory(Category)
--- app/routers/category.py
+++ app/routers/category.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.models import Category
 from app.schemas import CategoryCreate, CategoryRead, CategoryUpdate
 from app.crud.category import category
 from uuid import UUID
+import pandas as pd
 
 router = APIRouter(prefix="/categories", tags=["Categorys"])
 
@@ -12,22 +13,37 @@ router = APIRouter(prefix="/categories", tags=["Categorys"])
 def create_category(category_in: CategoryCreate, db: Session = Depends(get_db)):
     return category.create(db, category_in)
 
+@router.get("/types/{type}/sum")
+def sum_type(type: str, db: Session = Depends(get_db)):
+    category_list = category.get_categories_by_type(db, type)
+    if not category_list:
+        raise HTTPException(status_code=404, detail=f"No categories found for type '{type}'")
+    
+    file_list = [file for cat in category_list for file in cat.files]
+
+    sum = 0
+    for file in file_list:
+        excel = pd.ExcelFile(file.path)
+        for sheet in excel.sheet_names:
+            df = pd.read_excel(excel, sheet_name=sheet, header=None)
+            df = df.apply(pd.to_numeric, errors="coerce")
+            sum += df.sum().sum(skipna=True)
+
+    return sum
+
 # for testing TO BE DELETED
 @router.get("/", response_model=list[CategoryRead])
 def list_categorys(db: Session = Depends(get_db)):
     return category.get_all(db)
 
-
 # @router.get("/{category_id}", response_model=CategoryRead)
 # def read_category_by_id(category_id: UUID, db: Session = Depends(get_db)):
 #     return category.get(db, category_id)
-
 
 # @router.put("/{category_id}", response_model=CategoryRead)
 # def update_category(category_id: UUID, category_in: CategoryUpdate, db: Session = Depends(get_db)):
 #     existing = category.get(db, category_id)
 #     return category.update(db, existing, category_in)
-
 
 @router.delete("/{category_id}", response_model=CategoryRead)
 def delete_category(category_id: UUID, db: Session = Depends(get_db)):
