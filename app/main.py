000996af36f0fbd160ebf2c75a4b812283b9e598
Michal <michalzvu25@gmail.com>
2025-09-18 23:26:28 +0300

    explaining

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ from app.db.database import Base, engine
 app = FastAPI()
 router = APIRouter(prefix='/api')
 
-# Create tables automatically when FastAPI starts
-# todo change to alembic
+# if i had more time, instead of creating tables automatically when 
+# FastAPI starts, i would add a database migration tool, like alembic,
+# in order to manage db schema changes in a controlled & versioned way
 Base.metadata.create_all(bind=engine)
 
 # routes
