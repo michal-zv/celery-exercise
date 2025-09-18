e520e002b115bd5292634242be35e7879ecf9a0e
Michal <michalzvu25@gmail.com>
2025-09-18 17:05:58 +0300

    file path with ts + name

--- app/services/storage.py
+++ app/services/storage.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
-from uuid import uuid4
-from app.config import settings
+from datetime import datetime, timezone
 
 class LocalStorage:
     def __init__(self, base_dir: str):
@@ -9,10 +8,12 @@ class LocalStorage:
         os.makedirs(self.base_dir, exist_ok=True)
 
     # generate unique filename to avoid collisions
+    # use upload time as distinction
     # todo maybe change to id instead
     def save_file(self, file, filename: str) -> str:
-        extention = os.path.splitext(filename)[1]
-        unique_name = f"{uuid4()}{extention}"
+        name, extention = filename.rsplit('.', 1)
+        ts = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S%f")
+        unique_name = f"{name}-{ts}.{extention}"
         file_path = Path(self.base_dir) / unique_name
 
         with open(file_path, "wb") as f:
